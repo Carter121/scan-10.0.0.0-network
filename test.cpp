f99681f8618418191aba5082339ae04abfe76c96
Carter121 <cartercarling37@gmail.com>
2022-04-25 21:01:20 -0600

    first commit

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int starting;
+    cout << "Where do you want to start?" << endl;
+    cin >> starting;
+    cout << "Starting at 10." << starting << ".0.0" << endl;
+    for(int i = starting; i < 255; i++) {
+        string number = to_string(i);
+        string str = "nmap 10." + number + "0.0/16 -oN 10." + number + ".x.x";
+        const char *command = str.c_str();
+        system(command);
+        cout << command << endl;
+    }
+    return 0;
+}
